08040c29916577d8a57d0b1b788be48c3001d9d1
Daniel <d.akipskiy@gmail.com>
2025-04-19 12:14:08 +0800

    feat(main): добавь асинхронность

--- src/main.py
+++ src/main.py
@@ -1,40 +1,39 @@
-from typing import Optional, Callable
+import asyncio
+import logging
+import sys
+
+from selenium_driverless import webdriver
 
 import akp.root
-from akp.driver_shell import DriverShell
 from akp.logger import LOGGER
+from akp.selenium_driverless_ex import webdriver_ex
 from config.config import settings
 from llm.chatgpt import ChatGPT
 from llm.chatgpt_configuration import FLAGS
 
-
-class ConsoleCommand:
-    def __init__(self, name: str, func: Callable):
-        self._name = name
-        self._func = func
-
-    def execute(self, *args, **kwargs):
-        self._func(*args, **kwargs)
-
-    def get_name(self):
-        return self._name
+ROOT = akp.root.get_external_project_root()
 
 
-def main():
+async def start_driver():
+    browser_path = ROOT / "browser"
+    extensions_dir = browser_path / "extensions"
+    extensions = [
+        extensions_dir / "adguard"
+    ]
+
+    options = webdriver.ChromeOptions()
+    [options.add_extension(i) for i in extensions]
+    options.user_data_dir = browser_path / "user_data1"
+    options.headless = True
+
+    driver = await webdriver_ex.ChromeEx(options=options)
+    return driver
+
+
+async def main_async():
     LOGGER.enable(settings.chatgpt.logging.enabled == 1)
 
-    # while True:
-    #     input_text = input("Введите команду: ")
-
-    root = akp.root.get_project_root()
-
-    driver_user_data = root / "browser/user_data1"
-    # driver_extensions = (
-    #     str(root / "browser/extensions/adguard/5.0.183_0"),
-    #     str(root / "browser/extensions/hola/1.237.569_0")
-    # )
-    # driver_extensions_str = ", ".join(driver_extensions)
-    driver: Optional[DriverShell.Selenium] = None
+    driver = await start_driver()
 
     chat_gpt_config_name = settings.chatgpt.configuration.name
     chat_gpt_person_name = settings.chatgpt.personalization.name
@@ -45,30 +44,38 @@ def main():
         chat_gpt_flag_incognito = chat_gpt_config.value.get_flag(FLAGS.FLAG_INCOGNITO_MODE)
         chat_gpt_flag_new_chat = chat_gpt_config.value.get_flag(FLAGS.FLAG_START_NEW_CHAT)
 
-        driver = DriverShell.SeleniumBaseUC(user_data_dir=driver_user_data, headless=True, incognito=chat_gpt_flag_incognito)
-
         chat_gpt = ChatGPT(driver, enable_personalization=True)
         chat_gpt.set_config(chat_gpt_config)
         chat_gpt.set_personalization(chat_gpt_person)
 
-        if chat_gpt.RPA.open_main_page():
+        if await chat_gpt.rpa.open_main_page():
 
             if chat_gpt_flag_new_chat:
-                chat_gpt.RPA.new_chat()
+                await chat_gpt.rpa.new_chat()
+
+            LOGGER.info(f"Загружен {chat_gpt_config_name}, {chat_gpt_person.value.ai_character}")
 
             while True:
+
                 prompt = input("Введите промт: ")
                 if prompt == 'q':
                     break
 
-                chat_gpt.RPA.send_prompt(prompt)
-                print(f"Ответ: {chat_gpt.RPA.get_last_response(start_delay=1)}")
+                await chat_gpt.rpa.send_prompt(prompt)
+                print(f"Ответ: {await chat_gpt.rpa.get_last_response(start_delay=1)}")
 
     except OSError:
         pass
     finally:
         if driver:
-            driver.quit()
+            await driver.quit(clean_dirs=False)
+
+    return 0
+
+
+def main():
+    result = asyncio.run(main_async())
+    return result
 
 
 if __name__ == "__main__":
